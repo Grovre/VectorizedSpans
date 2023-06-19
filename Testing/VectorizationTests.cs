c4621f303fbafd9b6f3fbf7da640d4df248669e3
Grovre <50428844+Grovre@users.noreply.github.com>
2023-06-19 13:09:08 -0500

    Basic span enumerator and tests

---
+++ Testing/VectorizationTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+using VectorEnumerator;
+
+namespace Testing;
+
+[TestFixture]
+public class VectorizationTests
+{
+    private static int[] Numbers = new int[124_369];
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        var rand = new Random();
+        for (var i = 0; i < Numbers.Length; i++)
+        {
+            Numbers[i] = rand.Next(0, 1_000);
+        }
+    }
+    
+    [Test]
+    public void VectorizedSpanEnumeratorTest()
+    {
+        var copyList = new List<int>();
+        int Incrementer(int z) => z + Vector<int>.Count;
+        var enumerator = new VectorizedSpanEnumerator<int>(Numbers, Incrementer);
+        while (enumerator.MoveNext())
+        {
+            var v = enumerator.Current;
+            for (var i = 0; i < Vector<int>.Count; i++)
+            {
+                copyList.Add(v[i]);
+            }
+        }
+
+        foreach (var n in enumerator.Leftovers)
+        {
+            copyList.Add(n);
+        }
+
+        Assert.AreEqual(Numbers, copyList.ToArray());
+    }
+    
+    [Test]
+    public void EmptyVectorizedSpanEnumeratorTest()
+    {
+        var copyList = new List<int>();
+        int Incrementer(int z) => z + Vector<int>.Count;
+        var enumerator = new VectorizedSpanEnumerator<int>(Span<int>.Empty, Incrementer);
+        while (enumerator.MoveNext())
+        {
+            var v = enumerator.Current;
+            for (var i = 0; i < Vector<int>.Count; i++)
+            {
+                copyList.Add(v[i]);
+            }
+        }
+        
+        foreach (var n in enumerator.Leftovers)
+        {
+            copyList.Add(n);
+        }
+
+        Assert.AreEqual(Array.Empty<int>(), copyList.ToArray());
+    }
+}
---
+++ VectorEnumerator/VectorizedSpanEnumerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace VectorEnumerator;
+public ref struct VectorizedSpanEnumerator<T>
+    where T : unmanaged
+{
+    private readonly Span<T> _span;
+    private int _index;
+    private readonly Func<int, int> _indexIncrementer;
+
+    public Vector<T> Current => new Vector<T>(_span[_index..]);
+    public Span<T> Leftovers => _span[^(_span.Length % Vector<T>.Count)..];
+
+    public VectorizedSpanEnumerator()
+    {
+        _span = Span<T>.Empty;
+        _index = 0;
+        _indexIncrementer = _ => 0;
+    }
+
+    public VectorizedSpanEnumerator(Span<T> span, Func<int, int> indexIncrementer)
+    {
+        _span = span;
+        _index = -indexIncrementer(0);
+        _indexIncrementer = indexIncrementer;
+    }
+
+    public bool MoveNext()
+    {
+        var i = _indexIncrementer(_index);
+        if (i < 0 || i > _span.Length - Vector<T>.Count)
+            return false;
+
+        _index = i;
+        
+        return true;
+    }
+
+    public void Reset()
+    {
+        _index = 0;
+    }
+}
